cc47150a020c7b6c436d8ef7e87dee44964a02d3
V1centR <vicentcdb@gmail.com>
2024-01-20 15:41:50 -0300

    First commit + config file + application start OK

---
+++ src/main/java/com/schedulertm/SchedulertmApplication.java
@@ -0,0 +1,13 @@
+package com.schedulertm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulertmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulertmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/schedulertm/controller/SchedulerController.java
@@ -0,0 +1,18 @@
+package com.schedulertm.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/scheduler")
+public class SchedulerController {
+	
+	@CrossOrigin
+	@GetMapping("/all")
+	public String getAll() {
+		return "OK";
+	}
+
+}
